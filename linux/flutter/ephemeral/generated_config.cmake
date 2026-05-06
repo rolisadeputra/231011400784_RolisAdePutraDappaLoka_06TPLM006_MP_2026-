7918ce224a6bf319c75cef63e38cf3c7f3fb007f
Rolis <putradappaloka@gmail.com>
2026-05-06 23:00:18 +0800

    initial commit

---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,21 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/mnt/data/development/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/mnt/data/development/UTS_MP_ROLIS/workshop_rolis" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/mnt/data/development/flutter"
+  "PROJECT_DIR=/mnt/data/development/UTS_MP_ROLIS/workshop_rolis"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuNDEuNw==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049Y2MwNzM0YWM3MQ==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049NTlhYTU4NGZkZg==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMS41"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/mnt/data/development/UTS_MP_ROLIS/workshop_rolis/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/mnt/data/development/UTS_MP_ROLIS/workshop_rolis/lib/main.dart"
+)
